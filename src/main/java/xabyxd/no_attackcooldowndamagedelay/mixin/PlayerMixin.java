ef9ae94b9d5a0809595eb8d8bc03bd5a8299eea2
xabyxd <69438213+xabyxd@users.noreply.github.com>
2025-08-27 02:36:30 +0200

    Project setup and base code

---
+++ src/main/java/xabyxd/no_attackcooldowndamagedelay/mixin/PlayerMixin.java
@@ -0,0 +1,23 @@
+package xabyxd.no_attackcooldowndamagedelay.mixin;
+
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xabyxd.no_attackcooldowndamagedelay.Config;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    @Inject(method = "resetAttackStrengthTicker",at = @At("HEAD"),cancellable = true)
+    public void resetAttackStrengthTicker(CallbackInfo ci) {
+        if(Config.NoAttackCooldownValue)
+            ci.cancel();
+    }
+    @Inject(method = "getAttackStrengthScale",at = @At("HEAD"),cancellable = true)
+      public void getAttackStrengthScale(float p_36404_, CallbackInfoReturnable<Float> cir) {
+        if(Config.NoAttackCooldownValue)
+        cir.setReturnValue(1F);
+    }
+}
